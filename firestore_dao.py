39fe226fc34923353aeb647f240c447f7bf1256d
Dave Fennessey <vazheel@gmail.com>
2021-10-14 18:14:19 +0200

    removing old methods

--- firestore_dao.py
+++ firestore_dao.py
@@ -12,21 +12,6 @@ class FirestoreDao:
     def __init__(self):
         self.firestore_db = firestore.Client()
 
-    def get_example_search(self, search_term="arizona"):
-        doc_ref = self._get_search_reference(search_term)
-        doc = doc_ref.get()
-        return doc
-
-    def get_products(self, pid_list: list[str]):
-        ref_list = [self._get_product_reference(pid) for pid in pid_list]
-        return self.firestore_db.get_all(ref_list)
-
-    def _get_search_reference(self, search_term: str):
-        return self.firestore_db.collection(SEARCHES_COLLECTION).document(search_term)
-
-    def _get_product_reference(self, pid: str):
-        return self.firestore_db.collection(PRODUCTS_COLLECTION).document(pid)
-
     def get_all_searches(self):
         print("Fetching all searches")
         searches = self.firestore_db.collection(SEARCHES_COLLECTION).get()
